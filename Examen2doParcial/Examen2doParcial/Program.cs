92e6cd06468cf51ac656f6d48aed2ff794c55e7d
LuisFerCornejo <94479281+LuisFerCornejo@users.noreply.github.com>
2022-01-20 17:53:36 -0500

    Add files via upload

---
+++ Examen2doParcial/Examen2doParcial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Examen2doParcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instance.mensaje); //Aqui es donde va a mostrar Bienvenido Luis Ferna... , utilizando asi la la propiedad Instance, el constructor y la variable creada
+            Singleton.Instance.mensaje = "Fecha: 20/01/2022"; //Aqui hacemos uso de la propiedad INSTANCE para mostrar un segundo mensaje y como podemos ver usamos la misma propiedad 
+            Console.WriteLine(Singleton.Instance.mensaje);
+            Singleton.Instance.mensaje = "Curso: 3ro Nivel C";// Aquí mostramos el 3er mensaje utilizando como los anteriores la propiedad INSTANCE como lo vemos en la linea de codigo inferior.
+            Console.WriteLine(Singleton.Instance.mensaje);
+        }
+    }
+}
---
+++ Examen2doParcial/Examen2doParcial/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Aqui uso el patron de diseño Singleton para mostrar mis datos que va en e lencabezado de un examen
+//
+namespace Examen2doParcial
+{
+    public class Singleton
+    {
+        private static Singleton instance = null; //Privatisamos la clase Singleton con su atributo instance 
+        public string mensaje = ""; //Creamos mensaje para mostrar un mensaje en pantalla, en esta caso mis datos, pero por el momento no tiene nada
+
+        protected Singleton() //Creamos este constructor 
+        {
+            mensaje = "Nombre: Cornejo Macias Luis Fernando"; //Aqui es donde va a mostrar el primer mensaje que se encuentra en PROGRAM.CS
+        }
+
+        public static Singleton Instance //El beneficio de que utilicemos la propiedad INSTANCE de SINGLETON es que siempre será la misma
+        {                                // y por ende va ser el mismo para todo, pero aún así no es recomendable utilizarlo para todo.
+            get //Utilizamos el get para devolver a la clase instanciada 
+            {
+                if (instance == null)
+                    instance = new Singleton();
+
+                return instance; //Aqui devolvemos lo que contiene la propiedad INSTANCE
+            }
+        }
+    }
+}
